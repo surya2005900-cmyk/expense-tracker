7b6cd1ddd0fbab98299be351e005cf89470c050a
Surya <surya2005900@gmail.com>
2026-05-08 21:45:07 +0530

    fix github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/expense-tracker/',
+
   plugins: [
     react(),
     tailwindcss(),
